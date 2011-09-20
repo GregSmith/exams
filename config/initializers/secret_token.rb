f09771ef1447735a5bd3e273cb18b79a02ed1301
Greg Smith <greg.smith@alpha-score.com>
2011-09-19 21:49:57 -0700

    Products Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exams::Application.config.secret_token = '4c622fd9cf10e859a9bc56f7e4ac901155ab4c04062d22ee9815504465cdcd498638a8fde615a1280ea505587a3c7a2b6bdc1d813c463e048050d060f2ff8bc0'
---
+++ db/seeds.rb
@@ -0,0 +1,57 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Mayor.create(:name => 'Daley', :city => cities.first)
+Product.delete_all
+Product.create(:title => 'Actual LSAT 63 - June 2011 Prep Test',
+  :description => 
+    %{<p>
+        <em>Actual LSAT 63 - June 2011 Prep Test</em> includes the complete exam
+        as complete explanations for every question.
+      </p>},
+  :image_url =>   '/images/lsat_prep_test_63.jpg',    
+  :price => 49.95)
+# . . .
+Product.create(:title => 'Actual LSAT 62 - December 2010 Prep Test',
+  :description =>
+    %{<p>
+        <em>Actual LSAT 62 - December 2010 Prep Test</em> includes the complete exam
+        as complete explanations for every question.
+      </p>},
+  :image_url => '/images/lsat_prep_test_62.jpg',
+  :price => 49.95)
+# . . .
+
+Product.create(:title => 'Actual LSAT 61 - October 2010 Prep Test',
+  :description => 
+    %{<p>
+        <em>Actual LSAT 61 - October 2010 Prep Test</em> includes the complete exam
+        as complete explanations for every question.
+      </p>},
+  :image_url => '/images/lsat_prep_test_61.jpg',
+  :price => 49.95)
+  
+# . . .
+
+Product.create(:title => 'Actual LSAT 60 - June 2010 Prep Test',
+  :description => 
+    %{<p>
+        <em>Actual LSAT 60 - June 2010 Prep Test</em> includes the complete exam
+        as complete explanations for every question.
+      </p>},
+  :image_url => '/images/lsat_prep_test_60.gif',
+  :price => 49.95)
+
+# . . .
+
+Product.create(:title => 'Actual LSAT 59 - December 2009 Prep Test',
+  :description => 
+    %{<p>
+        <em>Actual LSAT 61 - December 2009 Prep Test</em> includes the complete exam
+        as complete explanations for every question.
+      </p>},
+  :image_url => '/images/lsat_prep_test_59.jpg',
+  :price => 49.95)

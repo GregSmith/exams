b5e3c903c450d9b5533f6c3403b99bd1b48ccc74
Greg Smith <greg.smith@alpha-score.com>
2011-09-22 19:02:17 -0700

    page 170

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -21,7 +21,7 @@ class OrdersControllerTest < ActionController::TestCase
       post :create, :order => @order.attributes
     end
 
-    assert_redirected_to order_path(assigns(:order))
+    assert_redirected_to store_path
   end
 
   test "should show order" do
@@ -46,4 +46,19 @@ class OrdersControllerTest < ActionController::TestCase
 
     assert_redirected_to orders_path
   end
+  
+  test "requires item in cart" do
+    get :new
+    assert_redirected_to store_path
+    assert_equal flash[:notice], 'Your cart is empty'
+  end
+    test "should get new" do
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    LineItem.create(:cart => cart, :product => products(:ruby))
+    
+    get :new
+    assert_response :success
+  end
+  
 end

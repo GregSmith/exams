93bfc4649fe128369fa6fb57215e76b42c524ddb
Greg Smith <greg.smith@alpha-score.com>
2011-09-22 19:09:23 -0700

    page 172

--- db/migrate/20110923020114_add_price_to_line_item.rb
+++ db/migrate/20110923020114_add_price_to_line_item.rb
@@ -1,6 +1,9 @@
 class AddPriceToLineItem < ActiveRecord::Migration
   def self.up
     add_column :line_items, :price, :decimal
+    LineItem.all.each do |li|
+      li.price = li.product.price
+    end
   end
 
   def self.down
